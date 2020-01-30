be26506853cbe6b0e485f92d91d0258d976f8569
Rebecca Hong <rebeccahongsf@gmail.com>
2020-01-29 22:31:30 -0800

    Turn searchbar from functional to class, add state

--- src/components/cardlist.js
+++ src/components/cardlist.js
@@ -1,11 +1,33 @@
-import React from 'react'
+// import React from 'react';
+// import { Link, graphql, StaticQuery } from 'gatsby';
 
-const cardlist = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+// const SEARCH_QUERY = graphql`
+// query SearchList($zipcode: String!) {
+//     search(term:"dog park",
+//            location: $zipcode) {
+//         business{
+//             name
+//             id
+//             rating
+//             url
+//         }
+//     }
+// }`
 
-export default cardlist;
+// const cardlist = () => {
+//     return (
+//         <StaticQuery 
+//             query={SEARCH_QUERY}
+//             render={({search}) => (
+//                 <h1>Dog Parks Nearby</h1>
+//                 <div>
+//                     {search.map(({business}) => (
+//                         <h2>{ business.name }</h2>
+//                     ))}
+//                 </div>
+//             )}
+//         />
+//     )
+// }
+
+// export default cardlist;
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -16,15 +16,43 @@ const Form = styled.form`
 `;
 
 
-const searchbar = () => {
-    return (
-        <div>
-            <form>
-                <input placeholder="Enter Zip Code" type="number" name="zipcode" maxLength="5" required />
-                <button type="submit" name="submit">Search</button>
-            </form>
-        </div>
-    )
+class searchbar extends React.Component {
+    state = {
+        zipcode: "",
+    }
+
+    handleInputChange = event => {
+        const target = event.target
+        const value = target.value
+        const name = target.name
+        this.setState({
+            [name]: value,
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        alert(`Your zipcode is ${this.state.zipcode}!`)
+    }
+
+    render() {    
+        return (
+            <div>
+                <Form onSubmit={this.handleSubmit}>
+                    <input 
+                        placeholder="Enter Zip Code" 
+                        type="number" 
+                        name="zipcode" 
+                        maxLength="5" 
+                        value={this.state.zipcode}
+                        onChange={this.handleInputChange}
+                        required 
+                    />
+                    <button type="submit" name="submit">Search</button>
+                </Form>
+            </div>
+        )
+    }
 }
 
 export default searchbar;
